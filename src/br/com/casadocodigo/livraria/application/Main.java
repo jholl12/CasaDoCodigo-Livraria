bb1df89bbe7d91de4f3b5a80690acffd9ebd1d94
#Jhonata Santos <jholl12@gmail.com>
2017-05-29 21:59:11 -0300

    Adicionado ação para o botão exportar csv

--- src/br/com/casadocodigo/livraria/application/Main.java
+++ src/br/com/casadocodigo/livraria/application/Main.java
@@ -1,11 +1,14 @@
 package br.com.casadocodigo.livraria.application;
 
+import java.io.FileNotFoundException;
+import br.com.casadocodigo.livraria.modelo.Exportador;
 import br.com.casadocodigo.livraria.modelo.RepositorioDeProdutos;
 import br.com.casadocodigo.livraria.modelo.produtos.Produto;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,7 +16,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -27,37 +29,58 @@ public class Main extends Application {
 		Scene cenario = new Scene(grupo, 690, 510);
 		primaryStage.setScene(cenario);
 
+		//Recupera lista de produtos e adiciona na tabela
 		ObservableList<Produto> produtos = new RepositorioDeProdutos().lista();
-		TableView tabela = new TableView(produtos);
+		TableView<Produto> tabela = new TableView<>(produtos);
 
+		//Cria coluna de NOME
 		TableColumn colunaNome = new TableColumn("Nome");
 		colunaNome.setMinWidth(180);
 		colunaNome.setCellValueFactory(new PropertyValueFactory("nome"));
 
+		//Cria coluna de DESCRICAO
 		TableColumn colunaDescricao = new TableColumn("Descrição");
 		colunaDescricao.setMinWidth(230);
 		colunaDescricao.setCellValueFactory(new PropertyValueFactory("descricao"));
 
+		//Cria coluna de VALOR
 		TableColumn colunaValor = new TableColumn("Valor");
 		colunaValor.setMinWidth(60);
 		colunaValor.setCellValueFactory(new PropertyValueFactory("valor"));
 
+		//Cria coluna de ISBN
 		TableColumn colunaIsbn = new TableColumn("ISBN");
 		colunaIsbn.setMinWidth(180);
 		colunaIsbn.setCellValueFactory(new PropertyValueFactory("isbn"));
 
+		//Adiciona as colunas na tabela
 		tabela.getColumns().addAll(colunaNome, colunaDescricao, colunaValor, colunaIsbn);
 
+		//Adiciona tabela em uma caixa para alinhar
 		VBox vbox = new VBox(tabela);
 		vbox.setPadding(new Insets(70, 0, 0, 10));
 
+		//Cria label
 		Label label = new Label("Listagem de Livros");
 		label.setFont(Font.font("Lucida Grande", FontPosture.REGULAR, 30));
 		label.setPadding(new Insets(20, 0, 10, 10));
 		
+		//Cria o botão
 		Button btnExportar = new Button("Exportar CSV");
 		btnExportar.setLayoutX(575);
-		btnExportar.setLayoutY(25);
+		btnExportar.setLayoutY(34);
+		
+		//Classe anônima dentro do parâmetro de ação do click
+		btnExportar.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event){
+				try {
+					new Exportador().exportarCSV(produtos);
+				} catch (FileNotFoundException e) {
+					throw new RuntimeException("Erro ao exportar arquivo: " + e);
+				}
+			}
+		});
 		
 		grupo.getChildren().addAll(label, vbox, btnExportar);
 
--- src/br/com/casadocodigo/livraria/modelo/Exportador.java
+++ src/br/com/casadocodigo/livraria/modelo/Exportador.java
@@ -12,40 +12,20 @@ public class Exportador {
 	 * 
 	 * @param produtos
 	 * @author Jhonata Santos
+	 * @throws FileNotFoundException 
 	 */
-	public void exportarCSV(List<Produto> produtos){
-		try {
-			PrintStream ps = new PrintStream("produtos.csv");
-			ps.println("Nome; Descrição; Valor; ISBN");
-			
-			for(Produto produto : produtos){
-				ps.println(String.format("%s; %s; %s; %s",
-						   produto.getNome(),
-						   produto.getDescricao(),
-						   produto.getValor(),
-						   produto.getIsbn()));
-			}
-			
-			ps.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+	public void exportarCSV(List<Produto> produtos) throws FileNotFoundException {
+		PrintStream ps = new PrintStream("produtos.csv");
+		ps.println("Nome; Descrição; Valor; ISBN");
+
+		for (Produto produto : produtos) {
+			ps.println(String.format("%s; %s; %s; %s", 
+					produto.getNome(), 
+					produto.getDescricao(), 
+					produto.getValor(),
+					produto.getIsbn()));
 		}
-		
+
+		ps.close();
 	}
-//	public static void main(String[] args){
-//		
-//		Livro livro = new LivroFisico(new Autor());
-//		livro.setNome("Java 8 Prático");
-//		livro.setDescricao("Novos recursos da linguagem");
-//		livro.setValor(59.90);
-//		livro.setIsbn("978-85-66250-46-6");
-//		
-//		Livro maisUmLivro = new LivroFisico(new Autor());
-//		maisUmLivro.setNome("Desbravando a O.O");
-//		maisUmLivro.setDescricao("Livro de Java e O.O");
-//		maisUmLivro.setValor(59.90);
-//		maisUmLivro.setIsbn("321-54-67890-11-2");
-//		
-//		new Exportador().exportarCSV(Arrays.asList(livro, maisUmLivro));
-//	}
 }
